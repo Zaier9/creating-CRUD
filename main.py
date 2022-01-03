ba614cb05dd0d036c638488ec2fbc01d7723afea
Zaier <veraalber989@gmail.com>
2022-01-03 13:26:12 -0300

    2022

--- main.py
+++ main.py
@@ -32,6 +32,16 @@ def delete_client(client_name):
     else:
         print('Client is not in clients list')
 
+def search_client(client_name):
+    clients_list = clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
 def _add_comma():
     global clients
     clients += ','
@@ -43,6 +53,7 @@ def _print_welcome():
     print('[C]reate client')
     print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
 
 def _get_client_name():
     return input('What is the client name? -> ')
@@ -57,14 +68,24 @@ if __name__ == '__main__':
         cliennt_name = _get_client_name()
         create_client(client_name)
         list_clients()
-    elif command == 'D':
-        client_name = _get_client_name()
-        delete_client(client_name)
+    elif command == 'L':
         list_clients()
     elif command == "U":
         client_name = _get_client_name()
         updated_client_name = input('What is the updated client name -> ')
         update_client(client_name, updated_client_name)
         list_clients()
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client is in the clients list')
+        else:
+            print('The client: {} is not in clients list'.format(client_name))
     else:
         print('Invalid command')
